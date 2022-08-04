4562245bd481daa2d32287788bfc76157c23d89f
Terry K <83253738+DeputyBerry@users.noreply.github.com>
2022-08-03 21:01:22 -0400

    initial commit

---
+++ random-meal-maker/script.js
@@ -0,0 +1,125 @@
+// run renderMeal() function on click of the button with class of random
+document.querySelector('button').addEventListener('click', renderMeal)
+
+
+const mealContainer = document.querySelector('.data')
+
+
+async function getMeals() {
+    const url = 'https://www.themealdb.com/api/json/v1/1/random.php'
+   try {
+    const res = await fetch(url)
+    return await res.json()
+   } catch (err){
+    console.log(err)
+   }
+}
+
+async function renderMeal() {
+    const meal = await getMeals()
+    // create a section for the meal
+    const mealSection = document.createElement('section')
+    // use the meal data to create a meal
+    // for each ingredient in called meal, create an li
+    let video = 'https://www.youtube.com/embed?v=A96hbwobKKs'
+    mealSection.innerHTML = `
+    <h2>${meal.meals[0].strMeal}</h2>
+    <ul>
+        <li>${meal.meals[0].strIngredient1} -
+            <span>${meal.meals[0].strMeasure1}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient2} - 
+            <span>${meal.meals[0].strMeasure2}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient3} - 
+            <span>${meal.meals[0].strMeasure3}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient4} - 
+            <span>${meal.meals[0].strMeasure4}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient5} - 
+            <span>${meal.meals[0].strMeasure5}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient6} - 
+            <span>${meal.meals[0].strMeasure6}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient7} - 
+            <span>${meal.meals[0].strMeasure7}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient8} - 
+            <span>${meal.meals[0].strMeasure8}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient9} - 
+            <span>${meal.meals[0].strMeasure9}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient10} -  
+            <span>${meal.meals[0].strMeasure10}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient11} -  
+            <span>${meal.meals[0].strMeasure11}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient12} -  
+            <span>${meal.meals[0].strMeasure12}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient13} -  
+            <span>${meal.meals[0].strMeasure13}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient14} -  
+            <span>${meal.meals[0].strMeasure14}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient15} -  
+            <span>${meal.meals[0].strMeasure15}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient16} -  
+            <span>${meal.meals[0].strMeasure16}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient17} -  
+            <span>${meal.meals[0].strMeasure17}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient18} -  
+            <span>${meal.meals[0].strMeasure18}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient19} -  
+            <span>${meal.meals[0].strMeasure19}</span>
+        </li>
+        <li>${meal.meals[0].strIngredient20} -  
+            <span>${meal.meals[0].strMeasure20}</span>
+        </li>
+    </ul>
+    <h2>Instructions:</h2>
+    <p>${meal.meals[0].strInstructions}</p>
+    <iframe width="560" height="315" src="${video}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    `
+    // if mealSection innerHTML already exists replace it with the new meal
+    if (mealContainer.innerHTML !== '') {
+        mealContainer.innerHTML = ''
+        mealContainer.appendChild(mealSection)
+    } else {
+        mealContainer.appendChild(mealSection)
+    }
+
+    // for each li, and its children, add a class of ingredient
+    const ingredients = document.querySelectorAll('li')
+    ingredients.forEach(ingredient => {
+        ingredient.classList.add('ingredient')
+    })
+    // for each span, and its children, add a class of measure
+    const measures = document.querySelectorAll('span')
+    measures.forEach(measure => {
+        measure.classList.add('measure')
+    })
+
+    // if the innertext of the class ingredient is empty, remove the li
+    const emptyIngredients = document.querySelectorAll('.ingredient')
+    emptyIngredients.forEach(ingredient => {
+        if (ingredient.innerText === '-' || ingredient.innerHTML === null) {
+            ingredient.remove()
+        }
+        
+    })
+
+
+    
+
+}
