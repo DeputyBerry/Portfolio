677a2e3303f22efa43ccb50790c37cdd20ad3475
Terry K <83253738+DeputyBerry@users.noreply.github.com>
2022-09-06 14:17:07 -0400

    added initial commit

---
+++ Food-Checker-App/main.js
@@ -0,0 +1,61 @@
+function getFetch(){
+    let inputValue = document.getElementById('food').value;
+    const url = `https://world.openfoodfacts.org/api/v0/
+    product/${inputValue}.json`
+
+    if(inputValue.length !== 12){
+        alert('Please enter a valid barcode');
+        return;
+    }
+
+    fetch(url)
+    .then(response => response.json()) // parse response as JSON
+    .then(data => {
+        console.log(data)
+        if(data.status === 1){
+            const item = new ProductInfo(data.product)
+            item.displayInfo()
+            item.listIngredients()
+        } else if(data.status === 0){
+            alert(`Product ${inputValue} not found`)
+        }
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
+
+class ProductInfo {
+    constructor(productData) { //pass in data.product
+        this.name = productData.product_name
+        this.ingredients = productData.ingredients
+        this.image = productData.image_url
+    }
+
+    displayInfo() {
+        document.getElementById('foodImage').src = this.image
+        document.getElementById('product-name').innerText = this.name
+    }
+
+    listIngredients() {
+        let tableList = document.getElementById('ingredient-table')
+
+        for(let key in this.ingredients) {
+            let newRow = tableList.insertRow(-1)
+            let newICell = newRow.insertCell(0)
+            let newVegetarianCell = newRow.insertCell(1)
+            let newVeganCell = newRow.insertCell(2)
+            let newIText = document.createTextNode(
+                this.ingredients[key].text
+            )
+            let vegetarianStatus = this.ingredients[key].vegetarian
+            let newVegetarianText = document.createTextNode(vegetarianStatus)
+            let veganStatus = this.ingredients[key].vegan
+            let newVeganText = document.createTextNode(veganStatus)
+
+            newICell.appendChild(newIText)
+            newVegetarianCell.appendChild(newVegetarianText)
+            newVeganCell.appendChild(newVeganText)
+        }
+    }
+}
